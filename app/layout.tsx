d788c90b57b57e5d1ae5f3cfa1f51f2115e2f042
mrstrange1708 <junaidsamishaik@gmail.com>
2025-07-02 18:18:03 +0530

    fixed the bug in the hosting

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.png" type="image/x-icon" />
       </head>
       <body className={inter.className} suppressHydrationWarning>
         <ThemeProvider
